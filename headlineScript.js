41316e3b6c11070b02d8d9f77dff9fcb3ab0c21a
Ben Anderson <bena@wanews.com.au>
2018-02-22 12:59:42 +0800

    streamlined arrays in JS file

--- headlineScript.js
+++ headlineScript.js
@@ -1,32 +1,35 @@
 function makeHeadline() {
-    var gina = {name: 'Gina Rinehart', image: 'gina'};
-    var ben = {name: 'Ben Cousins', image: 'ben'};
-    var quokka = {name: 'Quokka', image: 'quokka'};
-    var fifo = {name: 'FIFO worker', image: 'fifo'};
-    var sam = {name: 'Sam Kerr', image: 'sam'};
-    var nicnat = {name: 'Nic Nat', image: 'nicnat'};
-    var shark = {name: 'Shark', image: 'shark'};
-    var part1 = [];
-    part1.push(gina, ben, quokka, fifo, sam, nicnat, shark);
-    var part2 = ["gets married", "crashes ute", "takes nude selfie", "filmed making bizzare rant", "sculls stubbie of Emu Export", "named as One Nation candidate"];
-    var quay = {name: 'Elizabeth Quay', image: 'quay'};
-    var show = {name: 'Royal Show',image: 'show'};
-    var cottesloe = {name: 'Cottesloe Beach', image: 'cott'};
-    var adventure = {name: 'Adventure World', image: 'adventure'};
-    var rotto = {name: 'Rottnest', image: 'rotto'};
-    var bell = {name: 'Bell Tower', image: 'bell'};
-    var stadium = {name: 'Perth Stadium', image: 'stadium'};
-    var part3 = [];
-    part3.push(quay, show, cottesloe, adventure, rotto, bell, stadium);
-    var rand1 = Math.floor(Math.random()*part1.length);
-    var rand2 = Math.floor(Math.random()*part2.length);
-    var rand3 = Math.floor(Math.random()*part3.length);
-    var pic1 = part1[rand1].image; 
-    var pic2 = part3[rand3].image;
+    const array1 = [
+        {name: 'Gina Rinehart', image: 'gina'},
+        {name: 'Ben Cousins', image: 'ben'},
+        {name: 'Quokka', image: 'quokka'},
+        {name: 'FIFO worker', image: 'fifo'},
+        {name: 'Sam Kerr', image: 'sam'},
+        {name: 'Nic Nat', image: 'nicnat'}, 
+        {name: 'Shark', image: 'shark'}
+    ] ;
+    const array2 = ["gets married", "crashes ute", "takes nude selfie", "filmed making bizzare rant", "sculls stubbie of Emu Export", "named as One Nation candidate"];
+    const array3 = [
+        {name: 'Elizabeth Quay', image: 'quay'},
+        {name: 'Royal Show',image: 'show'},
+        {name: 'Cottesloe Beach', image: 'cott'},
+        {name: 'Adventure World', image: 'adventure'},
+        {name: 'Rottnest', image: 'rotto'},
+        {name: 'Bell Tower', image: 'bell'},
+        {name: 'Perth Stadium', image: 'stadium'}
+    ];
+    var rand1 = Math.floor(Math.random()*array1.length);
+    var rand2 = Math.floor(Math.random()*array2.length);
+    var rand3 = Math.floor(Math.random()*array3.length);
+    var part1 = array1[rand1];
+    var part2 = array2[rand2];
+    var part3 = array3[rand3];
+    var pic1 = part1.image; 
+    var pic2 = part3.image;
     var headlinePics = document.getElementById('pictures');
     headlinePics.innerHTML = '<img id="picture1" src="images/'+pic1+'.jpg">' + '<img id="picture2" src="images/'+pic2+'.jpg">';
     var headlineWords = document.getElementById('words');
-    headlineWords.innerHTML = part1[rand1].name + ' ' + part2[rand2] + ' ' + 'at' + ' ' + part3[rand3].name;
+    headlineWords.innerHTML = part1.name + ' ' + part2.name + ' ' + 'at' + ' ' + part3.name;
     }
 makeHeadline();
 
